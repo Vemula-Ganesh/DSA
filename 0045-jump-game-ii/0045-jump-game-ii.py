2a6ad8f483cf84e68e9ec7d5e64effb47c5c07e5
Ganesh_Vemula <148737177+Vemula-Ganesh@users.noreply.github.com>
2026-05-15 16:03:27 +0530

    Time: 4 ms (69.46%), Space: 20.1 MB (66.79%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,12 +1,12 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        # jump=currend=fast=0
-        # for i in range(len(nums)-1):
-        #     fast=max(fast,i+nums[i])
-        #     if i==currend:
-        #         jump+=1
-        #         currend=fast
-        # return jump
+        jump=currend=fast=0
+        for i in range(len(nums)-1):
+            fast=max(fast,i+nums[i])
+            if i==currend:
+                jump+=1
+                currend=fast
+        return jump
         n=len(nums)
         dp=[float("inf")] * n
         dp[0] = 0
