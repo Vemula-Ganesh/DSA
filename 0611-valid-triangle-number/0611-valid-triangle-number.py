331fa6cb0df3d238026405d0e9811a4d7d775bc3
Ganesh_Vemula <148737177+Vemula-Ganesh@users.noreply.github.com>
2026-05-18 14:48:39 +0530

    Time: 566 ms (29.38%), Space: 12.2 MB (99.05%) - LeetHub

---
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def triangleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        n=len(nums)
+        res=0
+        for k in range(2,n):
+            i=0
+            j=k-1
+            while i<j:
+                if nums[i]+nums[j]>nums[k]:
+                    res+=(j-i)
+                    j-=1
+                else:
+                    i+=1
+        return res
+
+
+        
